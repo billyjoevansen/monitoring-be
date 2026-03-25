38aece5dcd0fce87a5571d14398507d1b689ba85
billyjoevansen <billskyesketit@gmail.com>
2026-03-25 22:58:53 +0700

    fix app.py

--- app.py
+++ app.py
@@ -23,7 +23,6 @@ def create_app():
     raw_origins = os.getenv(
         'CORS_ORIGINS',
         'http://localhost:3000',
-        'http://127.0.0.1:3000'
     )
 
     # CORS — read origins from environment
